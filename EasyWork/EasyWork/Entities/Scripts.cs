5758b08116af17686fc9a69029734e555475a1f1
Dima <d.a.shimanov@gmail.com>
2016-09-11 20:52:29 +0700

    Реалзовал 5 классов для создания БД Реализовал подключение к БД

---
+++ EasyWork/EasyWork/Entities/Scripts.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyWork.Entities.BaseId;
+
+namespace EasyWork.Entities
+{
+    [Table("Scripts")]
+    public class Scripts : BaseIdEntity
+    {
+        [Required]
+        [StringLength(250)]
+        [Column(TypeName = "nvarchar")]
+        public string Description { get; set; }
+
+        [Required]
+        [StringLength(450)]
+        [Column(TypeName = "nvarchar")]
+        public string Script { get; set; }
+
+        [Required]
+        public DateTime DataAddition { get; set; }
+    }
+}
---
+++ EasyWork/EasyWork/Entities/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyWork.Entities.BaseId;
+
+namespace EasyWork.Entities
+{
+    public class User : BaseIdEntity
+    {
+        [Required]
+        [MinLength(5)]
+        [MaxLength(20)]
+        [Column(TypeName = "nvarchar")]
+        public string Login { get; set; }
+
+        [Required]
+        [MinLength(6)]
+        [MaxLength(15)]
+        [Column(TypeName = "nvarhar")]
+        public string Passord { get; set; }
+    }
+}
